9a67f795aabba8a8f762069a635051492ea90206
AndreTells <andresilvatelles@gmail.com>
2023-05-07 15:56:11 -0300

    arquivos referentes a projeto do visual studio removidos e adicao do makefile

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include "helper.h"
+#include "person.h"
+
+int main() {
+	init_structs();
+	int groups[2] = { 0,0 };
+
+	pthread_t t1,t2,t3,t4;
+
+	t1 = create_person(SERF, groups);
+	t2 = create_person(SERF, groups);
+	t3 = create_person(HACKER, groups);
+	t4 = create_person(HACKER, groups);
+
+	pthread_join(t1,NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+	pthread_join(t4, NULL);
+
+	destroy_structs();
+	return 0;
+}
